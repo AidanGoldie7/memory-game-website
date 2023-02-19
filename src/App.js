261a5bdf01cd439a99e2f505a271660d24774ce3
aidangoldie7 <40402670@live.napier.ac.uk>
2023-02-19 18:23:05 +0000

    3. Array of cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import './App.css'
 
+//adding the cards
+const cardImages = [
+  { "src": "img/croc.avif"},
+  { "src": "img/elephant.avif"},
+  { "src": "img/monkey.avif"},
+  { "src": "img/sloth.avif"},
+  { "src": "img/snake.avif"},
+  { "src": "img/tiger.avif"}
+]
+
 function App() {
+
+  //shuffle cards 
+  const shuffleCards = () => {
+    //making two instances of each card in the array 'cardImages'
+    const shuffleCards = [...cardImages, ...cardImages]
+      //shuffle the cards
+      .sort(() => Math.random() - 0.5)
+      //mapping cards and giving them an id
+      .map((card) => ({...card, id:Math.random() }))
+  }
+
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
